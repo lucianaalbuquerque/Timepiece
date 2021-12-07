f23e2d5288d2de30b2e1141b64671f5308d37e21
Luciana Albuquerque <lucianaalbuquerque12@gmail.com>
2021-12-07 23:26:19 +0000

    English Translation

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours() 
     var minuto = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora}:${minuto}.`
+    msg.innerHTML = `It's ${hora}:${minuto}.`
     if (hora >= 0 && hora < 12) {
         img.innerHTML = '<img src="morning.png">'
         document.body.style.background = "#ededa6"
+        msg.innerHTML += ' Good Morning!'
     } else if (hora >= 12 && hora < 18) {
         img.innerHTML = '<img src="afternoon.png">'
         document.body.style.background = "#95c4ac"
+        msg.innerHTML += ' Good Afternoon!'
     } else {
         img.innerHTML = '<img src="night.png">'
         document.body.style.background = "#948d9e"
+        msg.innerHTML += ' Good Night!'
     }
 }
-carregar()
